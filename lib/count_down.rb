24f67598a2312272694045286a7da12a0bd5c6aa
Jarrell Jackson <jdjack3@gmail.com>
2020-07-09 18:48:29 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,9 @@
 # Write your code here
+magic_exit_number = 1
 count = 10
 while count > 1 do
-  puts "Happy New Year!"
+  break if count == magic_exit_number
+  puts #{count}
   count -= 1
 end
+puts "Happy New Year!"
